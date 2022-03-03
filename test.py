ddbdefe49d7a2b4e874ee57e7a834ac39eef6461
dry.1208 <dry.1208@gmail.com>
2022-03-03 17:06:30 +0100

    nowy pomysł

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-x = 1
-y = 2
-print(x +y)
-# ttrrr
+import requests
+
+url = "https://www.wscad.com/HELP/Polish/WSCAD64/SUITE/50AddOns/Add-On_PI-Diagram.htm"
+raw_input = requests.get(url)
+text = raw_input.text
+print(text)
