7f6de606aad5ab691a764a0831c595e509c474da
dry.1208 <dry.1208@gmail.com>
2022-03-03 19:29:05 +0100

    zmiana 01

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import requests
 
-url = "https://www.wscad.com/HELP/Polish/WSCAD64/SUITE/50AddOns/Add-On_PI-Diagram.htm"
+url = "https://zajecia-programowania-xd.pl/flagi"
 raw_input = requests.get(url)
 text = raw_input.text
-print(text)
+lista_linii = text.split('</p>')
+for linia in lista_linii:
+    if '.pl' or '.com' in linia:
+        print(linia)
+
+            
